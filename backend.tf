a5fa83c5fae19ea624e68b6422ecd58d08ef1872
Claudinei Santos <neibaianinho@hotmail.com>
2023-10-30 19:29:42 -0300

    reafactor: alteracao em nome de bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "onetech-terraform-state"
+    bucket = "santosclaudinei-terraform-state"
     key    = "terraform.tfstate"
     region = "us-east-1"
     encrypt = true
